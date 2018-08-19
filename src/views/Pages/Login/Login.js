3fc2279eb1734754331a55d7d7e08778e56e012a
tungboom <tungpv1993@gmail.com>
2018-08-19 20:30:23 +0700

    Add files via upload

---
+++ src/views/Pages/Login/Login.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+// Auth actions
+import * as authActions from '../../../actions/authActions';
+// Child components
+import LoginForm from './LoginForm';
+import * as types from '../../../actions/authTypes';
+import { translate, Trans } from 'react-i18next';
+import {toastr} from 'react-redux-toastr';
+import history from './../../../history';
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleValidSubmit = this.handleValidSubmit.bind(this);
+    }
+
+    handleValidSubmit(event, values) {
+        this.props.actions.onGetToken(values).then((response) => {
+            const {access_token, refresh_token} = response.payload.data;
+            localStorage.setItem('access_token', access_token);
+            localStorage.setItem('refresh_token', refresh_token);
+            localStorage.setItem('is_authenticated', "true");
+
+            this.props.actions.onLogin().then((response) => {
+                localStorage.setItem('user', response.payload.data);
+            }).catch((response) => {
+                localStorage.clear();
+                toastr.error(this.props.t("auth.message.error.connectServer"));
+            });
+        }).catch((response) => {
+            localStorage.clear();
+            if (response.error !== undefined) {
+                if(response.error.response.data.error === "invalid_grant") {
+                    toastr.error(this.props.t("auth.message.error.wrongUsernamePassword"));
+                }
+            } else {
+                toastr.error(this.props.t("auth.message.error.connectServer"));
+            }
+        });
+    }
+
+    displayRedirectMessages() {
+        const isExpiredToken = localStorage.getItem('is_expired_token');
+        if (isExpiredToken === "true") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    getRedirectPath() {
+        const locationState = localStorage.getItem('current_location_state');
+        if (locationState) {
+            return locationState;
+        } else {
+            return '/';
+        }
+    }
+
+    render() {
+        let errorMessage = <div></div>;
+        if(this.displayRedirectMessages()) {
+            errorMessage = <div className="alert alert-danger"><Trans i18nKey="auth.message.error.needLogin"/></div>;
+        }
+        const isAuthenticated = localStorage.getItem('is_authenticated') === "true" ? true : false;
+        return (
+            isAuthenticated ?
+            <Redirect to={{
+                pathname: this.getRedirectPath(), state: { from: this.props.location }
+            }}/>
+            :
+            <LoginForm handleValidSubmit = {this.handleValidSubmit} errorMessage={errorMessage} />
+        )
+    }
+}
+
+
+function mapStateToProps(state, ownProps) {
+    return {
+        isAuthenticated: state.auth.isAuthenticated,
+        response: state.auth.login
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(authActions, dispatch)
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(translate('auth')(Login));
---
+++ src/views/Pages/Login/LoginForm.js
@@ -0,0 +1,161 @@
+import React, { Component } from 'react';
+import { Button, Card, CardBody, CardGroup, Col, Container, InputGroup, InputGroupAddon, InputGroupText, Row, FormGroup, DropdownMenu, DropdownToggle, Dropdown, DropdownItem } from 'reactstrap';
+import { AvForm, AvGroup, AvInput, AvFeedback } from 'availity-reactstrap-validation';
+import PropTypes from 'prop-types';
+import _debounce from 'lodash.debounce';
+import { translate, Trans } from 'react-i18next';
+import Config from '../../../config';
+
+class LoginForm extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.handleInvalidSubmit = this.handleInvalidSubmit.bind(this);
+        this.validateUsername = this.validateUsername.bind(this);
+        this.validatePassword = this.validatePassword.bind(this);
+        let language = sessionStorage.getItem('default_locale') ? sessionStorage.getItem('default_locale') : Config.defaultLocale;
+        this.state = {
+            dropdownOpen: new Array(1).fill(false),
+            dropDownValue: language === "vi" ? <Trans i18nKey="auth.button.langVi"/> : <Trans i18nKey="auth.button.langEn"/>,
+            errorUsername: "",
+            errorPassword: ""
+        };
+    }
+
+    toggle(i) {
+        const newArray = this.state.dropdownOpen.map((element, index) => {
+            return (index === i ? !element : false);
+        });
+        let language = sessionStorage.getItem('default_locale') ? sessionStorage.getItem('default_locale') : Config.defaultLocale;
+        this.setState({
+            dropdownOpen: newArray,
+            dropDownValue: language === "vi" ? <Trans i18nKey="auth.button.langVi"/> : <Trans i18nKey="auth.button.langEn"/>
+        });
+    }
+
+    handleInvalidSubmit(event, errors, values) {
+        //this.setState({errors, values});
+    }
+
+    validateUsername = _debounce((value, ctx, input, cb) => {
+        cb(true);
+        if (!value || value === '') {
+            this.setState({errorUsername: <Trans i18nKey="auth.message.username.required"/>});
+        } else if (value.length < 1 || value.length > 16) {
+            this.setState({errorUsername: <Trans i18nKey="auth.message.username.minMaxLength"/>});
+        } else if (value.match(/^[A-Za-z0-9]+$/)) {
+            this.setState({errorUsername: <Trans i18nKey="auth.message.username.pattern"/>});
+        }
+    }, 0);
+
+    validatePassword = _debounce((value, ctx, input, cb) => {
+        cb(true);
+        if (!value || value === '') {
+            this.setState({errorPassword: <Trans i18nKey="auth.message.password.required"/>});
+        } else if (value.length < 1 || value.length > 16) {
+            this.setState({errorPassword: <Trans i18nKey="auth.message.password.minMaxLength"/>});
+        } else if (value.match(/^[A-Za-z0-9]+$/)) {
+            this.setState({errorPassword: <Trans i18nKey="auth.message.password.pattern"/>});
+        }
+    }, 0);
+
+    render() {
+        const { t, i18n } = this.props;
+        const changeLanguage = (lng) => {
+            i18n.changeLanguage(lng);
+            sessionStorage.setItem('default_locale', lng);
+        }
+        return (
+            <div className="app flex-row align-items-center">
+                <Container>
+                <Row className="justify-content-center">
+                    <Col md="8">
+                    <CardGroup>
+                        <Card className="p-4">
+                        <CardBody>
+                            <FormGroup row>
+                                <Col md="9">
+                                    <h1><Trans i18nKey="auth.title.login"/></h1>
+                                    <p className="text-muted"><Trans i18nKey="auth.intro.login"/></p>
+                                </Col>
+                                <Col xs="12" md="3">
+                                    <Dropdown isOpen={this.state.dropdownOpen[0]} toggle={() => {
+                                        this.toggle(0);
+                                    }}  size="sm">
+                                    <DropdownToggle caret>
+                                        {this.state.dropDownValue}
+                                    </DropdownToggle>
+                                    <DropdownMenu>
+                                        <DropdownItem header><Trans i18nKey="auth.label.language"/></DropdownItem>
+                                        <DropdownItem onClick={() => changeLanguage('vi')}><Trans i18nKey="auth.button.langVi"/></DropdownItem>
+                                        <DropdownItem onClick={() => changeLanguage('en')}><Trans i18nKey="auth.button.langEn"/></DropdownItem>
+                                    </DropdownMenu>
+                                    </Dropdown>
+                                </Col>
+                            </FormGroup>
+                            {this.props.errorMessage}
+                            <AvForm onValidSubmit={this.props.handleValidSubmit} onInvalidSubmit={this.handleInvalidSubmit}>
+                                <AvGroup>
+                                    <InputGroup>
+                                        <InputGroupAddon addonType="prepend">
+                                            <InputGroupText><i className="fa fa-user"></i></InputGroupText>
+                                        </InputGroupAddon>
+                                        <AvInput id="username" name="username" placeholder={t('auth.placeholder.username')}
+                                            minLength="1" maxLength="16" pattern="^[A-Za-z0-9]+$" required 
+                                            validate={{async: this.validateUsername}} 
+                                        />
+                                        <AvFeedback>{this.state.errorUsername}</AvFeedback>
+                                    </InputGroup>
+                                </AvGroup>
+                                <AvGroup>
+                                    <InputGroup>
+                                        <InputGroupAddon addonType="prepend">
+                                            <InputGroupText><i className="fa fa-asterisk"></i></InputGroupText>
+                                        </InputGroupAddon>
+                                        <AvInput name="password" type="password" placeholder={t('auth.placeholder.password')}
+                                                minLength="1" maxLength="16" pattern="^[A-Za-z0-9]+$" required 
+                                            validate={{async: this.validatePassword}} 
+                                        />
+                                        <AvFeedback>{this.state.errorPassword}</AvFeedback>
+                                    </InputGroup>
+                                </AvGroup>
+                                <FormGroup className="form-actions">
+                                <Row>
+                                    <Col xs="6">
+                                        <Button type="submit" color="primary" className="px-4"><Trans i18nKey="auth.button.login"/></Button>
+                                    </Col>
+                                    <Col xs="6" className="text-right">
+                                        <Button type="button" color="link" className="px-0"><Trans i18nKey="auth.button.forgotPassword"/></Button>
+                                    </Col>
+                                </Row>
+                                </FormGroup>
+                            </AvForm>
+                        </CardBody>
+                        </Card>
+                        <Card className="text-white bg-primary py-5 d-md-down-none" style={{ width: 44 + '%' }}>
+                        <CardBody className="text-center">
+                            <div>
+                            <h2><Trans i18nKey="auth.title.signUp"/></h2>
+                            <p><Trans i18nKey="auth.intro.signUp"/></p>
+                            <Button color="primary" className="mt-3" active><Trans i18nKey="auth.button.register"/></Button>
+                            </div>
+                        </CardBody>
+                        </Card>
+                    </CardGroup>
+                    </Col>
+                </Row>
+                </Container>
+            </div>
+        );
+    }
+
+}
+
+LoginForm.propTypes = {
+    handleValidSubmit: PropTypes.func.isRequired,
+    errorMessage: PropTypes.object.isRequired
+};
+
+export default translate('auth')(LoginForm);
